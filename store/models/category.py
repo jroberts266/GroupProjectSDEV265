f7e52ad79c8c539f09f94b2818026794dbd65995
jroberts266 <jroberts266@ivytech.edu>
2024-04-17 22:57:50 -0400

    -changed all categories to house plant based categories -modified the database model to add a description field for each category -added descriptions to all the categories -added feature that displays description when category is hovered

--- store/models/category.py
+++ store/models/category.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name= models.CharField(max_length=50)
+    description = models.CharField(max_length=250, null=True)
 
     @staticmethod
     def get_all_categories():
